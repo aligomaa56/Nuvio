a0c77289f990415e4351b2aaec22f2d03d5e5666
Ali Gomaa <3looosh56565656@gmail.com>
2025-07-28 23:46:19 +0300

    Add authentication features with login and signup components
    - Created LoginAndSignupPage component for user authentication interface.
    - Implemented API routes for authentication handling using better-auth.
    - Developed auth client for social sign-in with Google.
    - Configured authentication settings with email/password and social providers.
    - Added utility functions for class name management.

---
+++ src/lib/auth.ts
@@ -0,0 +1,32 @@
+import { db } from '@/db/drizzle';
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import * as schema from '@/db/schema';
+import { nextCookies } from 'better-auth/next-js';
+// import { Resend } from 'resend';
+// import VerificationEmail from '@/components/emails/verification-email';
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      prompt: "select_account",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+    github: {
+      prompt: 'select_account',
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
+  database: drizzleAdapter(db, {
+    provider: 'pg',
+    schema,
+  }),
+  plugins: [nextCookies()],
+});
