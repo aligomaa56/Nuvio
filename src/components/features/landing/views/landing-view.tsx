d1131ce1c98dcb33b97d6023cd8c47607e6b86d7
Ali Gomaa <3looosh56565656@gmail.com>
2025-07-28 23:47:32 +0300

    Add landing page components for enhanced user engagement
    - Introduced CallToAction component to encourage user interaction with prominent buttons.
    - Created Features component to highlight key functionalities of the application.
    - Developed HeroSection component for an engaging introductory experience.
    - Implemented LandingView to assemble the landing page layout, integrating the new components.
    - Added Footer component for navigation and social media links, enhancing overall site structure.

---
+++ src/components/features/landing/views/landing-view.tsx
@@ -0,0 +1,17 @@
+import { HeroHeader } from '@/components/layout/header/header';
+import Footer from '@/components/layout/footer/footer';
+import HeroSection from '@/components/features/landing/components/hero-section';
+import Features from '@/components/features/landing/components/features';
+import CallToAction from '@/components/features/landing/components/call-to-action';
+
+export function LandingView() {
+  return (
+    <div>
+      <HeroHeader />
+      <HeroSection />
+      <Features />
+      <CallToAction />
+      <Footer />
+    </div>
+  );
+}
